89a145802650fc5b28c7390a1791e49525019434
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-07-21 10:41:51 +0200

    Add: #37 - created author entity, service and repository

---
+++ src/main/java/com/enzubis/bookshelf_spring/author/Author.java
@@ -0,0 +1,28 @@
+package com.enzubis.bookshelf_spring.author;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "authors")
+public class Author {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+
+    public Author(){}
+
+    public Author(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
