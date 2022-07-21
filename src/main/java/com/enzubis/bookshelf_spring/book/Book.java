16ef98dd830a29176e0d74568289043c5065c6d9
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-07-21 09:57:35 +0200

    Add: #37 - created book service and book repository

--- src/main/java/com/enzubis/bookshelf_spring/book/Book.java
+++ src/main/java/com/enzubis/bookshelf_spring/book/Book.java
@@ -8,7 +8,7 @@ public class Book {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
+    private Long id;
 
     @Column(name = "title")
     private String title;
@@ -30,11 +30,11 @@ public class Book {
         this.isbn = isbn;
     }
 
-    public int getId() {
+    public Long getId() {
         return id;
     }
 
-    public void setId(int id) {
+    public void setId(Long id) {
         this.id = id;
     }
 
