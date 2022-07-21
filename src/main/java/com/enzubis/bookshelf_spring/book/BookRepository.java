9e7fc319164bea7009f465241b2c2c2ef9cb8d53
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-07-21 20:57:21 +0200

    Mod: Added repository annotations

--- src/main/java/com/enzubis/bookshelf_spring/book/BookRepository.java
+++ src/main/java/com/enzubis/bookshelf_spring/book/BookRepository.java
@@ -1,6 +1,8 @@
 package com.enzubis.bookshelf_spring.book;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
 }
--- src/main/java/com/enzubis/bookshelf_spring/bookshelf/BookshelfRepository.java
+++ src/main/java/com/enzubis/bookshelf_spring/bookshelf/BookshelfRepository.java
@@ -1,4 +1,9 @@
 package com.enzubis.bookshelf_spring.bookshelf;
 
-public interface BookshelfRepository {
+import com.enzubis.bookshelf_spring.book.Book;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface BookshelfRepository extends JpaRepository<Bookshelf, Long> {
 }
