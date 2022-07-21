227bda84c6058fc57301a0d0b1d3f33f6b90778d
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-07-21 14:31:14 +0200

    Mod: Minor change

--- src/main/java/com/enzubis/bookshelf_spring/author/Author.java
+++ src/main/java/com/enzubis/bookshelf_spring/author/Author.java
@@ -1,7 +1,5 @@
 package com.enzubis.bookshelf_spring.author;
 
-import org.springframework.context.annotation.EnableMBeanExport;
-
 import javax.persistence.*;
 
 @Entity
@@ -13,7 +11,8 @@ public class Author {
 
     private String name;
 
-    public Author(){}
+    public Author() {
+    }
 
     public Author(String name) {
         this.name = name;
