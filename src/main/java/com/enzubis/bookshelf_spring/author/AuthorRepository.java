3bb06c041d68491d59aa07a7a5590aedda4f5551
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-07-21 10:46:10 +0200

    Revert "Add: #37 - created author entity, service and repository"
    This reverts commit 89a145802650fc5b28c7390a1791e49525019434.

--- src/main/java/com/enzubis/bookshelf_spring/author/AuthorRepository.java
+++
@@ -1,6 +0,0 @@
-package com.enzubis.bookshelf_spring.author;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AuthorRepository extends JpaRepository<Author, Long> {
-}
--- src/main/java/com/enzubis/bookshelf_spring/author/AuthorService.java
+++
@@ -1,4 +0,0 @@
-package com.enzubis.bookshelf_spring.author;
-
-public class AuthorService {
-}
