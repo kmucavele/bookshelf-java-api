96bf5d16cc3334806370521c96318d7b5bdfc31e
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-07-27 15:08:03 +0200

    Mod: Set final variables

--- src/main/java/com/enzubis/bookshelf_spring/author/AuthorService.java
+++ src/main/java/com/enzubis/bookshelf_spring/author/AuthorService.java
@@ -8,7 +8,7 @@ import java.util.List;
 @Service
 public class AuthorService {
 
-    private AuthorRepository authorRepository;
+    private final AuthorRepository authorRepository;
 
     @Autowired
     public AuthorService(AuthorRepository authorRepository){
