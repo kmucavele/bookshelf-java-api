889b5f54fd45b42fd09801e15da4a3665749ea03
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-07-27 15:13:16 +0200

    Mod: added get by uuid query

--- src/main/java/com/enzubis/bookshelf_spring/user/UserService.java
+++ src/main/java/com/enzubis/bookshelf_spring/user/UserService.java
@@ -1,9 +1,11 @@
 package com.enzubis.bookshelf_spring.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.data.mongo.ReactiveStreamsMongoClientDependsOnBeanFactoryPostProcessor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class UserService {
@@ -17,5 +19,26 @@ public class UserService {
 
     public List<User> getUsers() {
         return userRepository.findAll();
+    }
+
+    public User getUserByUUID(String uuid){
+        User user = userRepository.findUserByUuid(uuid);
+
+        try{
+            return user;
+        } catch (NullPointerException e){
+            throw new IllegalStateException("User does not exist! Please register first.");
+        }
+    }
+
+    public void addUser(String uuid){
+        User user = userRepository.findUserByUuid(uuid);
+
+        try{
+
+            userRepository.save(user);
+        } catch (NullPointerException e){
+            throw new IllegalStateException("User already exists!");
+        }
     }
 }
