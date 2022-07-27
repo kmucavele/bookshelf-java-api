c4b651e823a58627d31d1cb17d08b3f5167c68cc
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-07-27 15:32:57 +0200

    Mod: renamed bookshelfController & adjusted api paths

---
+++ src/main/java/com/enzubis/bookshelf_spring/bookshelf_api/BookshelfController.java
@@ -0,0 +1,32 @@
+package com.enzubis.bookshelf_spring.bookshelf_api;
+
+import com.enzubis.bookshelf_spring.book.Book;
+import com.enzubis.bookshelf_spring.user.User;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Set;
+
+@RestController
+@RequestMapping(path = "api/bookshelf")
+public class BookshelfController {
+
+    private final BookshelfServices bookshelfServices;
+
+    @Autowired
+    public BookshelfController(BookshelfServices bookshelfServices) {
+        this.bookshelfServices = bookshelfServices;
+    }
+
+    @GetMapping(path = "books")
+    public List<Book> getBooks() {
+        return bookshelfServices.getBooks();
+    }
+
+    @GetMapping(path = "{userId}/books")
+    public Set<Bookshelf> getUserBooks(@PathVariable("userId") String userId) {
+        bookshelfServices.addBookToUser(userId);
+        return bookshelfServices.getUserBooks(userId);
+    }
+}
