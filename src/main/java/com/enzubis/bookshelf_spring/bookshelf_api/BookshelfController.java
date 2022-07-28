a63a0be981e3e0f9327f95f1a28848f8b1398f16
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-07-28 09:38:17 +0200

    Mod: Added add book to user method

--- src/main/java/com/enzubis/bookshelf_spring/bookshelf_api/BookshelfController.java
+++ src/main/java/com/enzubis/bookshelf_spring/bookshelf_api/BookshelfController.java
@@ -1,14 +1,8 @@
 package com.enzubis.bookshelf_spring.bookshelf_api;
 
-import com.enzubis.bookshelf_spring.book.Book;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.List;
-import java.util.Set;
 
 @RestController
 @RequestMapping(path = "api/bookshelf")
@@ -21,14 +15,11 @@ public class BookshelfController {
         this.bookshelfServices = bookshelfServices;
     }
 
-    @GetMapping(path = "books")
-    public List<Book> getBooks() {
-        return bookshelfServices.getBooks();
-    }
 
-    @GetMapping(path = "{userId}/books")
+
+/*    @GetMapping(path = "{userId}/books")
     public Set<Bookshelf> getUserBooks(@PathVariable("userId") String userId) {
         bookshelfServices.addBookToUser(userId);
         return bookshelfServices.getUserBooks(userId);
-    }
+    }*/
 }
