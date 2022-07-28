a882bb1e6bfbefa6427d7dbe5393fa9a3d5dfe97
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-07-28 09:33:56 +0200

    Mod: user query returns optional

--- src/main/java/com/enzubis/bookshelf_spring/user/UserRepository.java
+++ src/main/java/com/enzubis/bookshelf_spring/user/UserRepository.java
@@ -3,7 +3,10 @@ package com.enzubis.bookshelf_spring.user;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, Long> {
-    User findUserByUuid(String uuid);
+    //User findUserByUuid(String uuid);
+    Optional<User> findUserByUuid(String uuid);
 }
