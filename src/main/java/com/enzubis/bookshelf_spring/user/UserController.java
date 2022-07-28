bb327dd32cdef83fc731c78d20b59003e035bb88
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-07-28 09:35:51 +0200

    Mod: Added new User route

--- src/main/java/com/enzubis/bookshelf_spring/user/UserController.java
+++ src/main/java/com/enzubis/bookshelf_spring/user/UserController.java
@@ -1,9 +1,7 @@
 package com.enzubis.bookshelf_spring.user;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -23,4 +21,8 @@ public class UserController {
         return userService.getUsers();
     }
 
+    @PostMapping(path = "/add")
+    public void addUser(@RequestBody User user) {
+        userService.addUser(user);
+    }
 }
