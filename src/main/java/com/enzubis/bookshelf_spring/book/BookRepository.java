2607a1c999ca1b1b8864f3d9515e1052605e73e0
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-07-29 11:30:40 +0200

    Mod: modified book query to isbn

--- src/main/java/com/enzubis/bookshelf_spring/book/BookRepository.java
+++ src/main/java/com/enzubis/bookshelf_spring/book/BookRepository.java
@@ -3,6 +3,12 @@ package com.enzubis.bookshelf_spring.book;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+import java.util.SimpleTimeZone;
+
 @Repository
 public interface BookRepository extends JpaRepository<Book, Long> {
+    // TODO: findByISBN
+
+    Optional<Book> findByIsbn(String isbn);
 }
