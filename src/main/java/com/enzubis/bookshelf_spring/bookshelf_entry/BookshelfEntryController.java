4bf5e992a1a743c6486a771f79e04d6dcac1c47c
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-08-02 16:42:23 +0200

    Mod: added first api doc descriptions

--- src/main/java/com/enzubis/bookshelf_spring/bookshelf_entry/BookshelfEntryController.java
+++ src/main/java/com/enzubis/bookshelf_spring/bookshelf_entry/BookshelfEntryController.java
@@ -4,6 +4,9 @@ import com.enzubis.bookshelf_spring.book.Book;
 import com.enzubis.bookshelf_spring.book.BookService;
 import com.enzubis.bookshelf_spring.user.User;
 import com.enzubis.bookshelf_spring.user.UserService;
+import com.fasterxml.jackson.annotation.JsonGetter;
+import io.swagger.annotations.ApiOperation;
+import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -26,11 +29,20 @@ public class BookshelfEntryController {
     }
 
     @GetMapping(path = "{userId}")
+    @ApiOperation(
+            value = "Returns the bookshelf entries of a user",
+            response = Book.class
+    )
     public List<BookshelfEntry> getUserBooks(@PathVariable("userId") String userId) {
         return bookshelfEntryServices.getUserBooks(userId);
     }
 
     @PostMapping(path = "/{userId}/add", consumes = "application/json")
+    @ApiOperation(
+            value = "Adds a new book to a user",
+            response = Book.class
+    )
+
     public void addBookEntry(
             @RequestBody Book book,
             @PathVariable("userId") String userId) {
