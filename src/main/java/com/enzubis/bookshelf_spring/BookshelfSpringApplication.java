299eb746346eef53f1f111f666b3bc627064ecae
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-08-02 15:11:03 +0200

    Mod: added swagger into project

--- src/main/java/com/enzubis/bookshelf_spring/BookshelfSpringApplication.java
+++ src/main/java/com/enzubis/bookshelf_spring/BookshelfSpringApplication.java
@@ -3,15 +3,14 @@ package com.enzubis.bookshelf_spring;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-import java.io.IOException;
-import java.util.concurrent.ExecutionException;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
 @SpringBootApplication
+@EnableSwagger2
 public class BookshelfSpringApplication {
 
-    public static void main(String[] args) throws IOException, ExecutionException, InterruptedException {
+    public static void main(String[] args) {
         SpringApplication.run(BookshelfSpringApplication.class, args);
     }
 }
