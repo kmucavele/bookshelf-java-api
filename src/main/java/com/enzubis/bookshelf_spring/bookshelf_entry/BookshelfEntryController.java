03c9cb0d3081116694a699018b70f582617baff6
Kenneth Mucavele <kenneth.mucavele@endava.com>
2022-08-04 14:46:15 +0200

    Mod: Added delete endpoint description

--- src/main/java/com/enzubis/bookshelf_spring/bookshelf_entry/BookshelfEntryController.java
+++ src/main/java/com/enzubis/bookshelf_spring/bookshelf_entry/BookshelfEntryController.java
@@ -4,9 +4,7 @@ import com.enzubis.bookshelf_spring.book.Book;
 import com.enzubis.bookshelf_spring.book.BookService;
 import com.enzubis.bookshelf_spring.user.User;
 import com.enzubis.bookshelf_spring.user.UserService;
-import com.fasterxml.jackson.annotation.JsonGetter;
 import io.swagger.annotations.ApiOperation;
-import io.swagger.annotations.ApiResponses;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,21 +26,20 @@ public class BookshelfEntryController {
         this.bookService = bookService;
     }
 
-    @GetMapping(path = "{userId}")
+
     @ApiOperation(
-            value = "Returns the bookshelf entries of a user",
-            response = Book.class
+            value = "Returns the bookshelf entries of a user"
     )
+    @GetMapping(path = "{userId}")
+
     public List<BookshelfEntry> getUserBooks(@PathVariable("userId") String userId) {
         return bookshelfEntryServices.getUserBooks(userId);
     }
 
     @PostMapping(path = "/{userId}/add", consumes = "application/json")
     @ApiOperation(
-            value = "Adds a new book to a user",
-            response = Book.class
+            value = "Adds a new book to a users bookshelf"
     )
-
     public void addBookEntry(
             @RequestBody Book book,
             @PathVariable("userId") String userId) {
@@ -53,13 +50,14 @@ public class BookshelfEntryController {
         bookshelfEntryServices.addBookToUser(user, addedBook);
     }
 
+    @ApiOperation(
+            value = "Deletes a book from a users bookshelf"
+    )
     @DeleteMapping(path = "/{userId}/{isbn}/delete")
     public void deleteBookEntry(@PathVariable("userId") String userId,
-                                @PathVariable("isbn") String isbn){
+                                @PathVariable("isbn") String isbn) {
         User user = userService.getUserByUUID(userId);
         Book book = bookService.getBookByIsbn(isbn);
         bookshelfEntryServices.deleteBookEntry(user, book);
     }
-
-
 }
